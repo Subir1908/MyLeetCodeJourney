88eed71c9a0f2466ebcc21b53b7abb7a6bce45cd
Subir Debnath <subirdebnath1908@gmail.com>
2026-08-13 01:18:20 +0530

    Completed Day 18

---
+++ Day 18/Find the Index of the First Occurrence in a String/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int i = 0, j = 0, k=0;
+        while(i<haystack.size() && j<needle.size()){
+            if(haystack[i]==needle[j]){
+                i++;
+                j++;
+            }
+            else{
+                i = i - j + 1;
+                j=0;
+            }
+        }
+        if(j==needle.size()){
+            return i-needle.size();
+        }
+        else{
+            return -1;
+        }
+    }
+};
---
+++ Day 18/readme.md
@@ -0,0 +1,3 @@
+Solved the Find the Index of the First Occurrence in a String problem.
+Used two pointer method for the solution and the worst time complexity of the algorithm is O(N * M) and space complexity is O(1).
+The final algoritm took 1ms of runtime and 9.22 MB of memory.
