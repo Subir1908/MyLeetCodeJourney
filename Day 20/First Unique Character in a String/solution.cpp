1585314ff195048f6588dccd0817f05c2988dcb3
Subir Debnath <subirdebnath1908@gmail.com>
2026-08-14 18:42:54 +0530

    Completed Day 20

---
+++ Day 20/First Unique Character in a String/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int freq[26] = {0};
+        for(int i=0;i<s.size();i++){
+            freq[s[i] - 'a']++;
+        }
+        for(int j=0;j<s.size();j++){
+            if(freq[s[j] - 'a'] == 1){
+                return j;
+            }
+        }
+        return -1;
+    }
+};
