a87a3680529aef59e86f24d984cdd2da33be849e
Subir Debnath <subirdebnath1908@gmail.com>
2026-08-23 16:13:46 +0530

    Completed Day 25

---
+++ Day 25/Plus One/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int i = n-1;
+        while(i>=0){
+            if(digits[i]==9){
+                digits[i] = 0;
+                i-=1;
+            }
+            else{
+                digits[i]+=1;
+                return digits;
+            }
+        }
+        if(i==-1){
+            digits[i+1]=1;
+            digits.push_back(0);
+        }
+        return digits;
+    }
+};
---
+++ Day 25/readme.md
@@ -0,0 +1,3 @@
+Completed the Plus One problem, where we have to add 1 to the large interger divided into a array.
+The final algorithm has a time complexity of O(N) and a space complexity of O(1).
+It has a runtime of 0ms and a 11.54 MB memory.
